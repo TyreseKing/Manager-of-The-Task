98ac95507dc4aa616e4c06a745aa7f3aed7d7d23
CinYP <111353883+CinYP@users.noreply.github.com>
2022-12-05 21:33:18 -0500

    Started working on server,js with basic app information to prep for back-end development

---
+++ sever.js
@@ -0,0 +1,22 @@
+//importing external module 
+const express = require('express');
+// app use express
+const app = express();
+//creating port 
+const PORT = process.env.PORT || 3001; 
+
+//using static assets through express middleweare 
+app.use(express.static('public'));
+// Middleware for the parsing of JSON data
+app.use(express.json());
+// Middleware for parsing of URL encoded data
+app.use(express.urlencoded({ extended: true }));
+
+require('./routes/htmlroutes')(app);
+//require('./routes/apiroutes')(app);
+
+app.listen(PORT, ()=>{
+    console.log('========================= ')
+    console.log(`Server available at ${PORT}`)
+    console.log('========================= ')
+}); 
