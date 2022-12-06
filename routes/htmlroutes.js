cdce71cb2ddefebcf667b708f4a37c8c707ed2e2
CinYP <111353883+CinYP@users.noreply.github.com>
2022-12-05 21:26:13 -0500

    updates to htmlroutes.js in routes directory.

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,1 +1,15 @@
 const path = require('path');
+
+module.exports = (app) => {
+    app.get('/', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+    app.get('/login', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../public/login.html'));
+    });
+
+    app.get('/task', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../public/task.html')); 
+    });
+}
