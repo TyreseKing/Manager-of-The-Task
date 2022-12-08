a273d5597bfaa63f7b49c888478355ffdc06fd02
CinYP <111353883+CinYP@users.noreply.github.com>
2022-12-07 20:24:35 -0500

    task.html edits | Added modal form for new task.

---
+++ public/assests/style.js
@@ -0,0 +1,5 @@
+const myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+// or
+//const myModalAlternative = new bootstrap.Modal('#myModal', options)
+
+addEventListener.myModal('click')
