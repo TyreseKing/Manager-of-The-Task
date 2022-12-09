81f71410ebccffc8a172e639903eb1dd3eadfe7f
CinYP <111353883+CinYP@users.noreply.github.com>
2022-12-08 22:26:29 -0500

    taskroutes.js updates

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,22 +5,3 @@ router.use('/api', apiRoutes);
 
 module.exports = router;
 
-
-
-
-// const path = require('path'); 
-// const fs = require('fs'); 
-// //var uniqid = require('uniqid');
-
-// module.exports = (app) => {
-//    app.get('/api/login', (req,res)=>{ }); 
-    
-//     app.get('/api/task', (req,res)=>{});
-
-//     app.post('/api/task', (req,res=>{}));
-
-//     app.delete('/api/task:id', (req, res)=> {});
-// };
-
-
-
