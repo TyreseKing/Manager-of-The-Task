1fa869f42f2adfdbae727baa6b8ad9ccf7af4a6c
CinYP <111353883+CinYP@users.noreply.github.com>
2022-12-12 20:12:14 -0500

    seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
 DROP DATABASE IF EXISTS tasks_db;
 CREATE DATABASE tasks_db;
 
-USE tasks_db;
+-- USE tasks_db;
 
-CREATE TABLE task (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    task_name VARCHAR(250) NOT NULL,
-    task_type VARCHAR(50) NOT NULL,
-    task_notes VARCHAR(500) 
-); 
+-- CREATE TABLE task (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+--     task_name VARCHAR(250) NOT NULL,
+--     task_type VARCHAR(50) NOT NULL,
+--     task_notes VARCHAR(500) 
+-- ); 
--- routes/api/userroutes.js
+++ routes/api/userroutes.js
@@ -1,21 +1,18 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection');
 const {user} = require('.../models'); 
 
-router.get('/', async(req, res) => {
-    try{
-        const userData = await user.findAll({
-            include: [{ model: user}], 
-            attributes: {
-                include: [
-                    [
-                        sequalize.literal(
-                            '(SELECT COUNT(*) FROM ' 
-                        )
-                    ]
-                ]
-            }
-        })
-    }
+router.post('/login', async(req, res) => {
+  try {
+    const const userData = await User.create(req.body);
+
+    req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+  }
 })
 
